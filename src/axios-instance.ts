349e103805ce5278042ff6384e3cbbc7b7bdede7
Oleh Kuper <ok1804@ukr.net>
2025-07-23 22:41:35 +0300

    update axios instance

--- src/axios-instance.ts
+++ src/axios-instance.ts
@@ -1,26 +1,16 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
-import Cookies from 'js-cookie';
-
-import { COOKIE_CONFIG } from './config';
-import { Tokens } from './enum';
-import { removeCookies } from './lib/utils';
-import { TTokens } from './types';
 
 const API_CONFIG = {
   baseURL: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:8000/api',
-  // withCredentials: true,
+  withCredentials: true, // Always send cookies
 } as const;
 
 const axiosInstance = axios.create(API_CONFIG);
 
+// Remove Authorization header logic entirely - rely only on cookies
 axiosInstance.interceptors.request.use(
   (config: InternalAxiosRequestConfig) => {
-    const accessToken = Cookies.get(Tokens.ACCESS);
-
-    if (accessToken) {
-      config.headers.Authorization = `Bearer ${accessToken}`;
-    }
-
+    // No Authorization header needed - cookies handle auth
     return config;
   },
   (error: AxiosError) => Promise.reject(error),
@@ -37,25 +27,18 @@ axiosInstance.interceptors.response.use(
       originalRequest._retry = true;
 
       try {
-        const refreshToken = Cookies.get(Tokens.REFRESH);
-
-        if (!refreshToken) {
-          throw new Error('No refresh token available');
-        }
-
-        const { data } = await axios.post<Pick<TTokens, 'accessToken'>>(
+        // Refresh endpoint will read refresh token from httpOnly cookies
+        await axios.post(
           `${API_CONFIG.baseURL}/refresh-token`,
-          { refreshToken },
+          {},
+          { withCredentials: true },
         );
 
-        Cookies.set(Tokens.ACCESS, data.accessToken, COOKIE_CONFIG);
-        axiosInstance.defaults.headers.common.Authorization = `Bearer ${data.accessToken}`;
-
+        // Retry original request - new access token cookie will be sent automatically
         return axiosInstance(originalRequest);
       } catch (refreshError) {
-        clearAuthTokens();
+        // Redirect to login if refresh fails
         window.location.href = '/login';
-
         return Promise.reject(refreshError);
       }
     } else {
@@ -63,10 +46,5 @@ axiosInstance.interceptors.response.use(
     }
   },
 );
-
-const clearAuthTokens = (): void => {
-  removeCookies();
-  delete axiosInstance.defaults.headers.common.Authorization;
-};
 
 export default axiosInstance;
